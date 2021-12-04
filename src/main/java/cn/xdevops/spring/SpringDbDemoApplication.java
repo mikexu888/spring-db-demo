52dbc99ad8c5161303d9fd3362b4e272e7c6b51a
mikexu888 <57089820+mikexu888@users.noreply.github.com>
2021-12-04 10:46:14 +0800

    Spring openshift demo
    Change something to follow the openshift wildfly

--- src/main/java/cn/xdevops/spring/SpringDbDemoApplication.java
+++ src/main/java/cn/xdevops/spring/SpringDbDemoApplication.java
@@ -2,9 +2,10 @@ package cn.xdevops.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringDbDemoApplication {
+public class SpringDbDemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDbDemoApplication.class, args);
